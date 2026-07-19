15dcb1ebf3d45c81c71eba301b9509fd4b08af40
ayush kumar <ayusha9538@gmail.com>
2026-07-19 13:07:41 +0530

    Day 2: Variables, Data Types and Input Output

---
+++ c++/arithmetic_operator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Arithmetic Operators "<<endl;
+    int a = 21;
+    int b = 10 ;
+    int sum = a+b;
+    int diff = a-b;
+    int mul = a*b;
+    double div = (double)a/b;
+    int rem = a%b;
+    cout<<"Sum: "<<sum<<endl;
+    cout<<"difference = "<<diff<<endl;
+    cout<<"multiplication = "<<mul<<endl;
+    cout<<"division = "<<div<<endl;
+    cout<<"remainder = "<<rem<<endl;
+    return 0; 
+
+}
---
+++ c++/input.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cin>>age;
+    cout<<"My age is "<<age<<endl;
+    return 0;
+
+}
